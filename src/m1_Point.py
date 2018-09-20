7ed38f7b121863ff990020820abaf8cd9418a505
kuznicmd <kuznicmd@rose-hulman.edu>
2018-09-20 15:36:14 -0400

    Version 2

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -60,7 +60,8 @@ class Point(object):
         return Point(self.x, self.y)
 
     def move_to(self, x, y):
-        self.distance = self.distance +
+        import math
+        self.distance = self.distance + math.sqrt((self.x - self.start_x) ** 2 + (self.y - self.start_y) ** 2)
         self.x = x
         self.y = y
         self.count = self.count + 1
